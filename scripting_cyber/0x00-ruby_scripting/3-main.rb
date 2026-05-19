26e69b891d3fb2d5b18ed54a329eb96ce91ea7c6
gulnisahlb <gulnisahlb@gmail.com>
2026-05-19 04:36:18 -0400

    hlb

---
+++ scripting_cyber/0x00-ruby_scripting/3-main.rb
@@ -0,0 +1,2 @@
+require_relative '3-read_file'
+count_user_ids('file.json')
---
+++ scripting_cyber/0x00-ruby_scripting/3-read_file.rb
@@ -0,0 +1,14 @@
+require 'json'
+ 
+def count_user_ids(path)
+  data = JSON.parse(File.read(path))
+ 
+  counts = Hash.new(0)
+  data.each do |entry|
+    counts[entry["userId"]] += 1
+  end
+ 
+  counts.sort.each do |user_id, count|
+    puts "#{user_id}: #{count}"
+  end
+end
