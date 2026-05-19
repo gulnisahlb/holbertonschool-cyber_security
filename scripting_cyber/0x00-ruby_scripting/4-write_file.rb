45cacf1b8c509e7497d24147e329db192f0c8397
gulnisahlb <gulnisahlb@gmail.com>
2026-05-19 04:39:35 -0400

    hlb

---
+++ scripting_cyber/0x00-ruby_scripting/4-write_file.rb
@@ -0,0 +1,10 @@
+require 'json'
+ 
+def merge_json_files(file1_path, file2_path)
+  data1 = JSON.parse(File.read(file1_path))
+  data2 = JSON.parse(File.read(file2_path))
+ 
+  merged = data2 + data1
+ 
+  File.write(file2_path, JSON.generate(merged))
+end
